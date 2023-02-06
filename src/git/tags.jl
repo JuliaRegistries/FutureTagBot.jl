ec0d2d3551a441a3cffd5af6c4908d7ebc761ed1
Dilum Aluthge <dilum@aluthge.com>
2023-02-06 17:24:58 -0500

    Update tags.jl

--- src/git/tags.jl
+++ src/git/tags.jl
@@ -11,6 +11,16 @@ function tag_already_exists(ctx::Context, version::VersionNumber)
     return !isempty(str)
 end
 
+function get_commit_for_existing_tag(ctx::Context, version::VersionNumber)
+    cloned_package = ctx.cloned_package
+    tag_name = _tag_name(version)
+    commit = cd(cloned_package.path) do
+        cmd = `git rev-parse $(tag_name)`
+        strip(read(cmd, String))
+    end
+    return commit
+end
+
 function create_tag(ctx::Context, plan::Plan)
     cloned_package = ctx.cloned_package
     tag_name = _tag_name(plan)
