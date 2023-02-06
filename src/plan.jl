0738594c7384777f829d6b99c10bd62c706e2a05
Dilum Aluthge <dilum@aluthge.com>
2023-02-06 15:39:31 -0500

    Update plan.jl

--- src/plan.jl
+++ src/plan.jl
@@ -1,7 +1,9 @@
 function generate_plan(ctx::Context, version::VersionNumber)
     target = version_to_target(ctx, version)
     candidate = version_to_candidate(ctx, version)
-    if candidate_satisfies_target(ctx, candidate, target)
+    if !candidate_satisfies_target(ctx, candidate, target)
+        msg = ""
+        throw(ErrorException(msg))
     end
     plan = Plan(;
         version,
