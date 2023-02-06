ad79362a96d65674e861ed68492dd68cf2e7bc4c
Dilum Aluthge <dilum@aluthge.com>
2023-02-06 18:20:43 -0500

    Update main.jl

--- src/main.jl
+++ src/main.jl
@@ -74,7 +74,7 @@ function github_release_single_version(ctx::Context, version::VersionNumber)
 
     # GitHub.create_release(gh_repo; params, auth = ctx.gh_auth)
 
-    cmd = `curl -vvvv --fail-with-body -X POST -H "Accept: application/vnd.github+json" -H "Authorization: Bearer $(ENV["GITHUB_TOKEN])"  -H "X-GitHub-Api-Version: 2022-11-28" https://api.github.com/repos/$(gh_repo_slug)/releases -d '{"tag_name":"$(tag_name)","target_commitish":"$(get_commit_for_existing_tag(ctx, version))","name":"$(tag_name)","generate_release_notes":true}'`
+    cmd = `curl -vvvv --fail-with-body -X POST -H "Accept: application/vnd.github+json" -H "Authorization: Bearer $(ENV["GITHUB_TOKEN"])" -H "X-GitHub-Api-Version: 2022-11-28" https://api.github.com/repos/$(gh_repo_slug)/releases -d '{"tag_name":"$(tag_name)","target_commitish":"$(get_commit_for_existing_tag(ctx, version))","name":"$(tag_name)","generate_release_notes":true}'`
     run(cmd)
 
     return nothing
