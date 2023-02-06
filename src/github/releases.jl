48b13b7bffca32eb0e80202fba868c8f7d9ff2ed
Dilum Aluthge <dilum@aluthge.com>
2023-02-06 17:07:25 -0500

    Update releases.jl

--- src/github/releases.jl
+++ src/github/releases.jl
@@ -1,4 +1,4 @@
-function github_release_exists(ctx::Context, plan::Plan)
+function github_release_already_exists(ctx::Context, plan::Plan)
     tag_name = _tag_name(plan)
     gh_repo_slug = ctx.cloned_package.package.gh_repo_slug
     gh_response = GitHub.gh_get_json(GitHub.DEFAULT_API, "/repos/$(gh_repo_slug)/releases/tags/$(tag_name)"; auth, handle_error = false)
