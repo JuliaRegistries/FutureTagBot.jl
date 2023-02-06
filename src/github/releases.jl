cd0bf6dfc335d90477734c9a25cd43073316d644
Dilum Aluthge <dilum@aluthge.com>
2023-02-06 17:36:55 -0500

    Update releases.jl

--- src/github/releases.jl
+++ src/github/releases.jl
@@ -2,5 +2,5 @@ function github_release_already_exists(ctx::Context, version::VersionNumber)
     tag_name = _tag_name(version)
     gh_repo_slug = ctx.cloned_package.package.gh_repo_slug
     gh_response = GitHub.gh_get_json(GitHub.DEFAULT_API, "/repos/$(gh_repo_slug)/releases/tags/$(tag_name)"; auth = ctx.gh_auth, handle_error = false)
-    return get(gh_response, "tag_name", nothing) == tag_name
+    return haskey(gh_response, "tag_name") && strip(gh_response["tag_name"]) == strip(tag_name)
 end
