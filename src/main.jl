d09c4cab9e33d53efe024449cc590c7c531dd2d2
Dilum Aluthge <dilum@aluthge.com>
2023-02-06 19:12:38 -0500

    Update main.jl

--- src/main.jl
+++ src/main.jl
@@ -75,7 +75,7 @@ function github_release_single_version(ctx::Context, version::VersionNumber)
     params["name"] = tag_name
     params["generate_release_notes"] = true
 
-    # GitHub.create_release(gh_repo; params, auth = ctx.gh_auth)
+    GitHub.create_release(gh_repo; params, auth = ctx.gh_auth)
 
     #data = "{\"tag_name\":\"$(tag_name)\",\"target_commitish\":\"$(commit_for_existing_tag)\",\"name\":\"$(tag_name)\",\"generate_release_notes\":true}"
     #cmd = `curl -vvvv --fail-with-body -X POST -H "Accept: application/vnd.github+json" -H "Authorization: Bearer $(ENV["GITHUB_TOKEN"])" -H "X-GitHub-Api-Version: 2022-11-28" https://api.github.com/repos/$(gh_repo_slug)/releases -d "$(data)"`
