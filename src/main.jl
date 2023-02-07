55095bb7fb41d4e5b86029c363b2c9fc29f7fec8
Dilum Aluthge <dilum@aluthge.com>
2023-02-06 19:09:44 -0500

    Update main.jl

--- src/main.jl
+++ src/main.jl
@@ -72,13 +72,14 @@ function github_release_single_version(ctx::Context, version::VersionNumber)
 
     # params = Dict()
     # params["tag_name"] = tag_name
-    # params["target_commitish"] = get_commit_for_existing_tag(ctx, version)
+    # params["target_commitish"] = commit_for_existing_tag
     # params["name"] = tag_name
     # params["generate_release_notes"] = true
 
     # GitHub.create_release(gh_repo; params, auth = ctx.gh_auth)
 
-    cmd = `curl -vvvv --fail-with-body -X POST -H "Accept: application/vnd.github+json" -H "Authorization: Bearer $(ENV["GITHUB_TOKEN"])" -H "X-GitHub-Api-Version: 2022-11-28" https://api.github.com/repos/$(gh_repo_slug)/releases -d '{"tag_name":"$(tag_name)","target_commitish":"237d0510bed0a276c1a348bc1f65b5d27f47757d","name":"$(tag_name)","generate_release_notes":true}'`
+    data = "{\"tag_name\":\"$(tag_name)\",\"target_commitish\":\"$(commit_for_existing_tag)\",\"name\":\"$(tag_name)\",\"generate_release_notes\":true}"
+    cmd = `curl -vvvv --fail-with-body -X POST -H "Accept: application/vnd.github+json" -H "Authorization: Bearer $(ENV["GITHUB_TOKEN"])" -H "X-GitHub-Api-Version: 2022-11-28" https://api.github.com/repos/$(gh_repo_slug)/releases -d "$(data)"`
     run(cmd)
 
     return nothing
